8a4917321847850d0a79f0eb1fafe7295003df26
lapandro <146387416+lapandro@users.noreply.github.com>
2025-06-03 18:39:42 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,23 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html", // مسح جميع ملفات HTML في مجلد public
+    "./src/**/*.js",     // مسح جميع ملفات JavaScript في مجلد src
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'], // تعريف خط Inter
+      },
+      colors: {
+        // يمكنك تعريف ألوانك المخصصة هنا
+        primary: '#F87171', // لون أحمر أساسي
+        secondary: '#FCA5A5', // لون أحمر ثانوي
+      }
+    },
+  },
+  plugins: [],
+}
+
 
