8ce971195e86d742e363edb5b1e2b214965a33e3
lapandro <146387416+lapandro@users.noreply.github.com>
2025-06-05 04:30:46 +0300

    Update authService.js

--- src/js/services/authService.js
+++ src/js/services/authService.js
@@ -7,3 +7,10 @@ export async function loginUser(email, password) {
   const userCredential = await signInWithEmailAndPassword(auth, email, password);
   return userCredential.user;
 }
+
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+export async function registerUser(email, password) {
+  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  return userCredential.user;
+}
