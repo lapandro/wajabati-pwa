b2b70eb703fc1e65814d85e4d394a6c574664cfb
lapandro <146387416+lapandro@users.noreply.github.com>
2025-06-05 19:22:07 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,22 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // القسم الأهم: يحدد المسارات التي يجب على Tailwind مسحها للبحث عن كلاسات CSS
-  // تأكد أن هذه المسارات تتطابق تمامًا مع مكان ملفاتك التي تستخدم كلاسات Tailwind
   content: [
-    // هذا المسار يشمل جميع ملفات .html داخل مجلد 'public' وجميع مجلداته الفرعية.
-    // على سبيل المثال: public/index.html
     "./public/**/*.html",
-    // هذا المسار يشمل جميع ملفات .js داخل مجلد 'src' وجميع مجلداته الفرعية.
-    // على سبيل المثال: src/js/main.js, src/js/components/**/*.js, src/js/views/**/*.js
     "./src/**/*.js",
   ],
   theme: {
     extend: {
-      // تعريف خط مخصص اسمه 'inter' لاستخدامه في Tailwind
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
+        tajawal: ['Tajawal', 'sans-serif'], // تأكد من إضافة خط Tajawal هنا
       },
-      // تعريف ألوان مخصصة إذا أردت استخدامها ككلاسات Tailwind
       colors: {
-        primary: '#F87171', // لون أحمر أساسي
-        secondary: '#FCA5A5', // لون أحمر ثانوي
+        primary: '#F87171',
+        secondary: '#FCA5A5',
       }
     },
   },
-  plugins: [], // إضافة أي إضافات (plugins) لـ Tailwind هنا إذا لزم الأمر
+  plugins: [],
 }
 
