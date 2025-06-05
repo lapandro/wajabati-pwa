f6cc80cfabbc632b57e8d8a82d3383c7cde325f7
lapandro <146387416+lapandro@users.noreply.github.com>
2025-06-05 03:35:30 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -5,7 +5,9 @@ echo 'document.addEventListener("DOMContentLoaded", () => {
     <p>هذا المحتوى تم تحميله بنجاح من <code>main.js</code> ✅</p>
     <button id="helloBtn" style="margin-top: 1rem; padding: 10px 20px; font-size: 16px;">جربني 👋</button>
   `;
-  document.getElementById("helloBtn").addEventListener("click", () => {
+ 
+console.log("🔥 تم النشر التلقائي من GitHub إلى Firebase!");
+ document.getElementById("helloBtn").addEventListener("click", () => {
     alert("مرحبًا بك! 👋 هذا الزر يعمل!");
   });
 });' > public/main.js
